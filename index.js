9f18e084c9917e00892b391b18d852fa616f44ce
Kevin M <64878242+kevinemueller@users.noreply.github.com>
2023-04-03 13:51:14 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Sonic {
 
     Jogging() {
         this.maxFrames = 8;
-        this.imgSrc = `.\\sprites\\${this.index}.png`;
+        this.imgSrc = `.\\sprites\\jogging\\${this.index}.png`;
     }
 }
 
